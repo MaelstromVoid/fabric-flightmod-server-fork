95e575616b29e5172acd6f91108a11f8ddb30919
MaelstromVoid <Disquo.Very@gmail.com>
2025-03-16 15:45:11 +0100

    Add command options removeAll and list to /fly

--- src/main/java/ewewukek/flightmod/FlyCommand.java
+++ src/main/java/ewewukek/flightmod/FlyCommand.java
@@ -32,6 +32,11 @@ public class FlyCommand {
                                     ServerPlayerEntity target = EntityArgumentType.getPlayer(context, "player");
                                     return removeFlyPlayer(context.getSource(), target);
                                 })))
+                .then(literal("removeAll")
+                        .executes(context -> removeFlyPlayers(context.getSource())))
+                .then(literal("list")
+                        .executes(context -> GetAllowedPlayerList(context.getSource())))
+
         );
     }
 
@@ -39,10 +44,10 @@ public class FlyCommand {
         String playerName = player.getName().getString();
 
         if (Config.playersAllowedToFly.contains(playerName)) {
-            source.sendFeedback(Text.of(player.getName().getString() + " can already fly !"), false);
+            source.sendFeedback(Text.of(playerName + " can already fly !"), false);
         } else {
             Config.playersAllowedToFly.add(playerName);
-            source.sendFeedback(Text.of( player.getName().getString() + " can now fly !"), true);
+            source.sendFeedback(Text.of(playerName + " can now fly !"), true);
             Config.playersAllowedToFlyHashMap.clear();
             Config.save();
         }
@@ -54,14 +59,31 @@ public class FlyCommand {
         String playerName = player.getName().getString();
 
         if (!Config.playersAllowedToFly.contains(playerName)) {
-            source.sendFeedback(Text.of(player.getName().getString() + " can't fly."), false);
+            source.sendFeedback(Text.of(playerName + " can't fly."), false);
         } else {
             Config.playersAllowedToFly.remove(playerName);
-            source.sendFeedback(Text.of(player.getName().getString() + " can't fly anymore !"), true);
+            source.sendFeedback(Text.of(playerName + " can't fly anymore !"), true);
             Config.playersAllowedToFlyHashMap.clear();
             Config.save();
         }
 
+        return 1;
+    }
+
+    private static int removeFlyPlayers(ServerCommandSource source) {
+        Config.playersAllowedToFly.clear();
+        Config.playersAllowedToFlyHashMap.clear();
+        Config.save();
+        source.sendFeedback(Text.of("List of players allowed to fly has been clean"), true);
+        return 1;
+    }
+
+    private static int GetAllowedPlayerList(ServerCommandSource source) {
+        if (Config.playersAllowedToFly.isEmpty()) {
+            source.sendFeedback(Text.of("List of players allowed to fly is empty"), false);
+        } else {
+            source.sendFeedback(Text.of("All players allowed to fly: " + String.join(",", Config.playersAllowedToFly)), false);
+        }
         return 1;
     }
 }
